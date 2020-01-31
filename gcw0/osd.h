40b4d262d7f638aa3486379fc4d3348dfb32185e
Panja0 <Panja0@users.noreply.github.com>
2020-01-31 17:05:42 +0100

    Update osd.h

--- gcw0/osd.h
+++ gcw0/osd.h
@@ -18,21 +18,21 @@
 #include "fileio.h"
 
 #define MAXPATHLEN 1024
-#define DEFAULT_PATH  "/.genplus"
+#define DEFAULT_PATH  "/.genplus_sg"
 #define osd_input_update sdl_input_update
 
 
-#define GG_ROM      "/usr/local/home/.genplus/lock-on/ggenie.bin"
-#define AR_ROM      "/usr/local/home/.genplus/lock-on/areplay.bin"
-#define SK_ROM      "/usr/local/home/.genplus/lock-on/sk.bin"
-#define SK_UPMEM    "/usr/local/home/.genplus/lock-on/sk2chip.bin"
-#define CD_BIOS_US  "/usr/local/home/.genplus/bios/bios_CD_U.bin"
-#define CD_BIOS_EU  "/usr/local/home/.genplus/bios/bios_CD_E.bin"
-#define CD_BIOS_JP  "/usr/local/home/.genplus/bios/bios_CD_J.bin"
-#define MD_BIOS     "/usr/local/home/.genplus/bios/bios_MD.bin"
-#define MS_BIOS_US  "/usr/local/home/.genplus/bios/bios_U.sms"
-#define MS_BIOS_EU  "/usr/local/home/.genplus/bios/bios_E.sms"
-#define MS_BIOS_JP  "/usr/local/home/.genplus/bios/bios_J.sms"
-#define GG_BIOS     "/usr/local/home/.genplus/bios/bios.gg"
+#define GG_ROM      "/usr/local/home/.genplus_sg/lock-on/ggenie.bin"
+#define AR_ROM      "/usr/local/home/.genplus_sg/lock-on/areplay.bin"
+#define SK_ROM      "/usr/local/home/.genplus_sg/lock-on/sk.bin"
+#define SK_UPMEM    "/usr/local/home/.genplus_sg/lock-on/sk2chip.bin"
+#define CD_BIOS_US  "/usr/local/home/.genplus_sg/bios/bios_CD_U.bin"
+#define CD_BIOS_EU  "/usr/local/home/.genplus_sg/bios/bios_CD_E.bin"
+#define CD_BIOS_JP  "/usr/local/home/.genplus_sg/bios/bios_CD_J.bin"
+#define MD_BIOS     "/usr/local/home/.genplus_sg/bios/bios_MD.bin"
+#define MS_BIOS_US  "/usr/local/home/.genplus_sg/bios/bios_U.sms"
+#define MS_BIOS_EU  "/usr/local/home/.genplus_sg/bios/bios_E.sms"
+#define MS_BIOS_JP  "/usr/local/home/.genplus_sg/bios/bios_J.sms"
+#define GG_BIOS     "/usr/local/home/.genplus_sg/bios/bios.gg"
 
 #endif /* _OSD_H_ */
